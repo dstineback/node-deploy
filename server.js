81495b24a07869738c2bb377c950c854df1c8680
Dan Stineback <dstineback@gmail.com>
2016-06-02 15:24:19 -0700

    save

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const errorHandler = require(__dirname + '/lib/error_handling');
 
 const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
+console.log('dbPort', dbPort);
 mongoose.connect(dbPort);
 
 const dogRouter = require(__dirname + '/routes/dog-route');
@@ -23,4 +24,4 @@ app.use(errorHandler);
 app.use((req, res)=> {
   res.status(404).json({msg: 'page not found test'});
 });
-app.listen(process.env.PORT || 3000, () => console.log('server is up on 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('server is up on ' + process.env.PORT || 3000));
